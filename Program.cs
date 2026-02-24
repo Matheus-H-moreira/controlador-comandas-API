786a5960465eec8e81866136b14a4c8a4c12150a
Matheus Moreira <matheushmoreira695@gmail.com>
2026-02-24 12:30:25 -0300

    Funcionamento do Banco de Dados

--- Program.cs
+++ Program.cs
@@ -1,18 +1,27 @@
+using Controlador_de_comandas.Data;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+//Configura o contexto do banco de dados para usar SQLite, especificando o arquivo de banco de dados
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=controlador_comandas.db"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+//Garantir que o banco de dados seja criado ao iniciar a aplicação
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
+
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-    app.MapScalarApiReference();
+        app.MapOpenApi();
+        app.MapScalarApiReference();
 }
 
 app.MapGet("/hello", () => "Hello World!");
