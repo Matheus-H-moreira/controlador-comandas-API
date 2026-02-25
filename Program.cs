f5adf3c9d5597ab39a251b74cc430d212a506a0f
Matheus Moreira <matheushmoreira695@gmail.com>
2026-02-25 11:44:45 -0300

    Criação dos primeiros EndPoints

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 using Controlador_de_comandas.Data;
+using Controlador_de_comandas.Models;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+
 //Configura o contexto do banco de dados para usar SQLite, especificando o arquivo de banco de dados
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite("Data Source=controlador_comandas.db"));
@@ -24,6 +26,17 @@ if (app.Environment.IsDevelopment())
         app.MapScalarApiReference();
 }
 
-app.MapGet("/hello", () => "Hello World!");
+app.MapPost("/mesas", async (Mesa mesa, AppDbContext db) =>
+{
+    db.Mesas.Add(mesa);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/mesas/{mesa.Id}", mesa);
+});
+
+app.MapGet("/mesas", async (AppDbContext db) =>
+{
+    return await db.Mesas.ToListAsync();
+});
 
 app.Run();
